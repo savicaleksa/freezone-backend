bb56608d499d967f90d757ab20ccc992fb46189c
Aleksa Savic <aleksa.savic91@gmail.com>
2022-08-03 20:40:25 +0200

    added benefits

---
+++ src/api/pogodnost/controllers/pogodnost.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  pogodnost controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::pogodnost.pogodnost');
---
+++ src/api/pogodnost/routes/pogodnost.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pogodnost router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::pogodnost.pogodnost');
